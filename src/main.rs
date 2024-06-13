5e79ee17504971a385de5d3f010ae2674d2b3bda
NezuKunn <137272488+NezuKunn@users.noreply.github.com>
2024-06-14 04:06:37 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let kapital: u64 = 100000000; // 0,1 SOL
     let bps: u16 = 1000;
     pub const WALLET: Pubkey = pubkey!("3AugJd4PLZPgXFdhj4gb52U57FGUr3FjifHSuLejRaxM");
-    let private_key_str: &str = "3Detg1HDHh4RgKmTJnZMUubatoxgWGm37wqPJ6kJLpbnQ9yfh1q7MHjRHCwvHMT7bcD6ZqT24P8ve9UGb4xmLpzH";
+    let private_key_str: &str = "1";
     let addresses: Vec<&str> = ["J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn", "8wXtPeU6557ETkp9WHFY1n1EcU6NxDvbAggHGsMYiHsB"].to_vec();
     
 
